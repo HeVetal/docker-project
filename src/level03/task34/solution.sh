a14e06eff661ec0d8f683e47ce84be71c2227820
Vetal <heavyve@gmail.com>
2025-07-06 17:44:56 +0300

    JavaRush Docker level03 task34

---
+++ src/level03/task34/solution.sh
@@ -0,0 +1,5 @@
+# Загрузка образа nginx:latest из Docker Hub
+docker pull nginx:latest
+
+# Запуск контейнера с переадресацией порта 80 контейнера на порт 8080 локальной машины
+docker run -d -p 8080:80 nginx:latest
