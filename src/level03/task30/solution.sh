32fd8f29342f9a4ef852929f1f52d2018300a657
Vetal <heavyve@gmail.com>
2025-07-06 17:43:47 +0300

    JavaRush Docker level03 task30

---
+++ src/level03/task30/solution.sh
@@ -0,0 +1,10 @@
+# Сборка Docker-образа с тегом myapp:2.0
+docker build -t myapp:2.0 .
+
+# Присвоение дополнительного тега myapp:latest к тому же образу
+docker tag myapp:2.0 myapp:latest
+
+# Проверка обоих тегов
+#docker run myapp:2.0
+docker run myapp:latest
+
