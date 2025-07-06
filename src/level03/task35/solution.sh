ad5e80fa488ef904143d1f4cba8f52694868c108
Vetal <heavyve@gmail.com>
2025-07-06 17:45:09 +0300

    JavaRush Docker level03 task35

---
+++ src/level03/task35/solution.sh
@@ -0,0 +1,8 @@
+# Построение обновленного образа
+docker tag yourusername/mynodeapp:1.0 yourusername/mynodeapp:2.0
+
+# Аутентификация в Docker Hub (это потребует ввода учетных данных)
+
+
+# Публикация образа в Docker Hub
+docker push yourusername/mynodeapp:2.0
