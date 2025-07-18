526713b351340772140fc4d04c26ba6a84f73879
Vetal <heavyve@gmail.com>
2025-07-18 09:47:31 +0300

    JavaRush Docker level05 task04

--- src/ru/javarush/python/fullstack/level05/task04/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task04/solution.sh
@@ -1,11 +1,11 @@
 # Инициализация Docker Swarm
-
+docker swarm init
 
 # Создание overlay-сети
-
+docker network create -d overlay my_overlay_network
 
 # Развертывание сервиса с контейнером Nginx в overlay-сети
-
+docker service create --name my_service --network my_overlay_network nginx
 
 # Проверка работы сервиса
-
+docker service ls
