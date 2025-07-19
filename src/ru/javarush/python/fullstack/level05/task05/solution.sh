df3fae73455d51310569bb78724ed43940166609
Vetal <heavyve@gmail.com>
2025-07-19 22:15:56 +0300

    JavaRush Docker level05 task05

--- src/ru/javarush/python/fullstack/level05/task05/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task05/solution.sh
@@ -1,10 +1,11 @@
 # Создание новой сети с драйвером bridge
-
+docker network create --driver bridge my_bridge_network
 
 # Запуск контейнера с Nginx и подключение его к сети
-
+docker -d run --name conteimer1 --network my_bridge_network nginx
 
 # Запуск контейнера с Redis и подключение его к сети
-
+docker -d run --name conteimer1 --network my_bridge_network redis
 
 # Проверка работы контейнеров в сети
+docker network inspect my_bridge_network
