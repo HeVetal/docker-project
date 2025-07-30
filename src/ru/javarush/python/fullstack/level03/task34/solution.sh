93d3c13d9d3d795d6dd7d0459e90f343efaadb5d
HeVetal <heavyve@gmail.com>
2025-07-30 19:00:50 +0300

    add JavaRush docker level 3 task34

--- src/ru/javarush/python/fullstack/level03/task34/solution.sh
+++ src/ru/javarush/python/fullstack/level03/task34/solution.sh
@@ -2,4 +2,4 @@
 docker pull nginx:latest
 
 # Запуск контейнера с переадресацией порта 80 контейнера на порт 8080 локальной машины
-docker run -d -p 8080:80 nginx:latest
+docker run -d -p 8080:80 --name my_nginx nginx:latest
