fbeace88acaebda5f87cb3ceff4be21aa07945cb
Vetal <heavyve@gmail.com>
2025-08-25 21:12:22 +0300

    JavaRush Docker level05 task08

--- src/ru/javarush/python/fullstack/level05/task08/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task08/solution.sh
@@ -1,10 +1,11 @@
 # Создание сети с драйвером bridge
-
+docker network create --driver bridge my_bridge
 
 # Запуск контейнера и подключение его к сети
-
+docker run -d --name container1 --network my_bridge
 
 # Отключение контейнера от сети
-
+docker network disconnect my_bridge container1
 
 # Удаление созданной сети
+docker network rm my_bridge
