b949d79fcf657adf54fc4c8449c35038051f97a1
Vetal <heavyve@gmail.com>
2025-08-27 17:13:32 +0300

    JavaRush Docker level04 task33

--- src/ru/javarush/python/fullstack/level04/task33/solution.sh
+++ src/ru/javarush/python/fullstack/level04/task33/solution.sh
@@ -1,10 +1,11 @@
 # Запуск контейнеров
-
+docker compouse up -d
 
 # Пауза для ожидания запуска контейнера
 sleep 10
 
 # Подключение и проверка данных
-
+docker exec -it postgres_container psql -U exampleuser -d exampledb -c "\l"
 
 # Остановка контейнеров после проверки
+docker compouse down
