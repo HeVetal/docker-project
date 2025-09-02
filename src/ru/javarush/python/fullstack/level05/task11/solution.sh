e456e9de2f11cf87c08cd2072e1d2bd7f799de60
Vetal <heavyve@gmail.com>
2025-09-02 16:21:49 +0300

    JavaRush Docker level05 task11

--- src/ru/javarush/python/fullstack/level05/task11/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task11/solution.sh
@@ -1,10 +1,11 @@
 # Создание пользовательской сети с драйвером bridge.
-
+docker network create --driver bridge my_bridge
 
 # Запуск контейнера с Nginx, подключенного к пользовательской сети.
-
+docker run -d --name web_server --network my_bridge nginx
 
 # Запуск контейнера с Busybox, подключенного к пользовательской сети.
-
+docker run -d --name app_server --network my_bridge busybox sleep 3600
 
 # Тестирование связи по имени хоста со стороны контейнера Busybox.
+docker exec app_server ping -c 4 web_server
