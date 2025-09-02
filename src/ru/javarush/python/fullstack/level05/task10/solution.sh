870532d9ebe5e9a9bdc0157a4ec76c3959e39989
Vetal <heavyve@gmail.com>
2025-09-02 16:21:44 +0300

    JavaRush Docker level05 task10

--- src/ru/javarush/python/fullstack/level05/task10/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task10/solution.sh
@@ -5,7 +5,7 @@ docker network create --driver bridge my_bridge
 docker run -d --name container1 --network my_bridge nginx
 
 # Запуск контейнера с Busybox и подключение к созданной сети
-docker run -d --name container2 --network my_bridge busybox
+docker run -d --name container2 --network my_bridge busybox sleep 3600
 
 # Проверка связи между контейнерами с использованием имени контейнера
 docker exec container2 ping -c 4 container1
