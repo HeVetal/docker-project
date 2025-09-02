3787621c9b797cd19dabc6f7c0e04aaa389b9491
Vetal <heavyve@gmail.com>
2025-09-02 16:21:54 +0300

    JavaRush Docker level05 task12

--- src/ru/javarush/python/fullstack/level05/task12/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task12/solution.sh
@@ -1,4 +1,9 @@
 # Запуск контейнеров с помощью Docker Compose
-
+docker compose up -d
+sleep 3
+winpty docker exec nginx_cont apt-get update
+winpty docker exec nginx_cont apt-get install -y iputils-ping
 
 # Проверка связи между веб-сервером и базой данных
+winpty docker exec nginx_cont ping -c 4 postgres_cont
+sleep 10
