0aa3bd32320f779491184c1cc822eef96e4d3c0b
Vetal <heavyve@gmail.com>
2025-09-04 16:10:21 +0300

    JavaRush Docker level06 task21

---
+++ src/ru/javarush/python/fullstack/level06/task21/solution.sh
@@ -0,0 +1,10 @@
+# Создаем секрет для пароля базы данных PostgreSQL
+docker swarm init
+docker swarm init --force-new-cluster 2>/dev/null || echo "Swarm уже инициализирован"
+docker secret rm db_password
+docker secret create db_password ./db_password.txt
+docker secret ls
+
+# Создаем сервис PostgreSQL с использованием созданного секрета
+docker service create --name postgres --secret db_password -e POSTGRES_PASSWORD_FILE=/run/secrets/db_password postgres
+sleep 10
