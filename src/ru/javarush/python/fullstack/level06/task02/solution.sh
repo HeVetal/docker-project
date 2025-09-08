9a9a634fd556f0f4300c1039b9414aa4e7f65f53
Vetal <heavyve@gmail.com>
2025-09-08 14:47:18 +0300

    JavaRush Docker level06 task02

--- src/ru/javarush/python/fullstack/level06/task02/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task02/solution.sh
@@ -1,18 +1,13 @@
 # Создаем директорию  /home/user/website
-mkdir -p  C/home/user/website
+mkdir -p  /home/user/website
 
 # Запустите контейнер с Nginx, смонтировав локальную директорию /home/user/website
 # в директорию /usr/share/nginx/html контейнера
-docker run -d --name my_cont_dir -v C/home/user/website:/usr/share/nginx/html nginx
+docker run -d --name my_cont_dir -v /home/user/website:/usr/share/nginx/html -p 8080:80 nginx
 
 # Проверьте, что контейнер запущен и директория смонтирована
-docker inspect my_cont_dir
+docker inspect my_cont_dir |grep 'Mounts'
 sleep 3
-
-#docker: Error response from daemon: C\home\user\website;C%!(EXTRA string=is not a valid Windows path).
-#See 'docker run --help'.
-#[]
-#Error: No such object: my_cont_dir
 
 
 
