1f7d2b3df3043b1170477f5d4fb6f4ed9d5bb621
Vetal <heavyve@gmail.com>
2025-09-08 15:37:12 +0300

    JavaRush Docker level07 task05

--- src/ru/javarush/python/fullstack/level07/task05/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task05/solution.sh
@@ -1,8 +1,10 @@
 # Сборка Docker-образа
-
+docker build -t my_image .
 
 # Запуск Docker-контейнера
-
+docker run --rm -d --name test_memory my_image
 
 # Мониторинг операций ввода/вывода
-
+docker stats test_memory --no-stream --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.BlockIO}}"
+echo '----x-----'
+sleep 7
