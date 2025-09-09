78e1db4e212b91ee231217bd1805288976d25459
Vetal <heavyve@gmail.com>
2025-09-09 16:16:59 +0300

    JavaRush Docker level07 task06

--- src/ru/javarush/python/fullstack/level07/task06/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task06/solution.sh
@@ -1,7 +1,8 @@
 # Запуск контейнера с Nginx
-
+docker run -d --name my_nginx -p 8080:80 nginx
 
 # Запуск контейнера с PostgreSQL
-
+docker run -d --name my_db_post -e POSTGRES_PASSWORD=pgpwd4habr  postgres
 
 # Мониторинг операций CPU и памяти
+docker stats
