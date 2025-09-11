971b1a0d12f44b0be970022a9f9b49720582672f
Vetal <heavyve@gmail.com>
2025-09-11 16:12:42 +0300

    JavaRush Docker level07 task09

--- src/ru/javarush/python/fullstack/level07/task09/solution.sh
+++ src/ru/javarush/python/fullstack/level07/task09/solution.sh
@@ -1,8 +1,12 @@
 # Запуск нескольких контейнеров
-docker run -d --name nginx_metric nginx
+docker run -d --name mongo_DB_metric mongo
+docker run -d --name redis_metric redis
 
 # Сбор метрик с использованием docker stats
-
+docker stats --no-stream --format "table {{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}"
 
 # Оптимизация потребляемых ресурсов
-
+docker stop $(docker ps -q)
+docker run -d -m 512 --name mongo_DB_metric mongo
+docker run -d --cpus="0.5" --name redis_metric redis
+sleep 6
