4f7c836dcfa84e65678918aa4d1ed49f2c830de3
Vetal <heavyve@gmail.com>
2025-09-18 15:23:50 +0300

    JavaRush Docker level05 task24

--- src/ru/javarush/python/fullstack/level05/task24/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task24/solution.sh
@@ -1,7 +1,8 @@
 # Инициализация Docker Swarm 
-
+docker swarm init --advertise-addr 192.168.1.10
 
 # Загрузите последний образ Nginx
-
+docker pull nginx:latest
 
 # Обновите сервис my_web, чтобы он использовал последнюю версию образа Nginx
+docker service update --image nginx:latest --update-order stop-first my_web
