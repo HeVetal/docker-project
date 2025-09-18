eac5aa4d7e9fe8f91d05ed2b6a55b9acd7b21a08
Vetal <heavyve@gmail.com>
2025-09-18 15:23:40 +0300

    JavaRush Docker level05 task22

--- src/ru/javarush/python/fullstack/level05/task22/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task22/solution.sh
@@ -1,1 +1,2 @@
 # Присоединяем рабочий узел с IP-адресом 192.168.1.20 к уже инициализированному кластеру Swarm
+docker swarm join --token SWMTKN-1-0defghi1234jklmnop-5678qrstuvwx90yz 192.168.1.10:2377
