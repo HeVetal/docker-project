c0dd7a2b1a2f8f356216553a1f61c8b0bed85723
Vetal <heavyve@gmail.com>
2025-09-20 16:15:54 +0300

    JavaRush Docker level05 task28

--- src/ru/javarush/python/fullstack/level05/task28/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task28/solution.sh
@@ -1,10 +1,13 @@
 # Инициализация Docker Swarm
-
+docker swarm init
 
 # Создание сети
-
+docke newtwork create -d overlay my_overlay_network
 
 # Развертывание стека с помощью Docker Compose
-
+docker stack deploy -c docker-compose.yml mystack
 
 # Проверка развернутых сервисов
+docker stack services mystack
+
+sleep 5
