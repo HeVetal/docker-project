beebc3f87f288af31bb5cfa73fae4a436efef179
Vetal <heavyve@gmail.com>
2025-09-20 16:15:59 +0300

    JavaRush Docker level05 task29

--- src/ru/javarush/python/fullstack/level05/task29/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task29/solution.sh
@@ -1,11 +1,14 @@
 # Создание сети
-
+docker network create -d bridge webnet
 
 # Запуск контейнера web с Nginx, подключенного к сети webnet
-
+docker run -d --name web --network webnet nginx
+docker exec web apt-get update
+docker exec web apt-get install -y iputils-ping
 
 # Запуск контейнера db с PostgreSQL, подключенного к сети webnet
-
+docker run -d --name db --network webnet -e POSTGRES_PASSWORD=mypass postgres
 
 # Проверка связи с помощью ping
-
+docker exec -it web ping -c 4 db
+sleep 5
