5fd92dc37870b596d572c2cb121f874d66dfade7
Vetal <heavyve@gmail.com>
2025-09-22 16:17:14 +0300

    JavaRush Docker level06 task05

--- src/ru/javarush/python/fullstack/level06/task05/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task05/solution.sh
@@ -2,7 +2,7 @@
 docker volume create db_backup
 
 # Запуск временного контейнера с использованием тома db_backup
-docker run -d --name my_db_backup -p 8080:80 -v db_backup:/usr/share/nginx/html nginx
+docker run --rm -v db_backup:/data busybox
 
 # Получение информации о томе db_backup
 docker volume inspect db_backup
