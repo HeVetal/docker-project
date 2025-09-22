03c9208b6c3034a864eb9118dd029b1dc4dbc6ee
Vetal <heavyve@gmail.com>
2025-09-22 16:17:01 +0300

    JavaRush Docker level05 task36

--- src/ru/javarush/python/fullstack/level05/task36/solution.sh
+++ src/ru/javarush/python/fullstack/level05/task36/solution.sh
@@ -1,1 +1,6 @@
 # Запустите контейнер с Nginx, смонтировав сертификаты и ключи для использования TLS
+docker run -d --name my_secure_container -p 443:443 \
+  -v /docker/cert.pem:/etc/nginx/cert.pem \
+  -v /docker/key.pem:/etc/nginx/key.pem \
+  -v /docker/nginx.conf:/etc/nginx/nginx.conf \
+  nginx
