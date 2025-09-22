a4f9df41944ce5f88f956e01909998ac190702d1
Vetal <heavyve@gmail.com>
2025-09-22 16:17:20 +0300

    JavaRush Docker level06 task07

--- src/ru/javarush/python/fullstack/level06/task07/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task07/solution.sh
@@ -6,8 +6,8 @@ docker volume craete my_vol3
 
 # Создаем и запускаем контейнеры, использующие эти тома
 docker run -d --name cont1 -p 8080:80 -v my_vol1:/app/data nginx
-docker run -d --name cont2 -p 8080:80 -v my_vol2:/app/data postgres
-docker run -d --name cont3 -p 8080:80 -v my_vol3:/app/data hello-world
+docker run -d --name cont2 -p 5432:5432 -v my_vol2:/app/data postgres
+docker run -d --name cont3 -v my_vol3:/app/data hello-world
 
 
 # Останавливаем контейнеры
@@ -17,9 +17,7 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps --all --quiet)
 
 # Очищаем неиспользуемые тома
-docker volume prune
+docker volume prune -f
 
 # Подтверждение удаления томов
 echo "Неиспользуемые тома успешно удалены."
-
-#тома остаются
