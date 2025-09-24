e52835c1e7df002da730c8f437ff6c3e6e6a8f9c
Vetal <heavyve@gmail.com>
2025-09-24 14:23:46 +0300

    JavaRush Docker level06 task10

--- src/ru/javarush/python/fullstack/level06/task10/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task10/solution.sh
@@ -2,7 +2,5 @@
 docker volume create readonly_volume
 
 # Запустите контейнер с Nginx, монтируя том readonly_volume в директорию /app в режиме "только для чтения"
-docker run -d --rm --name my_cont_readonly --mount source=readonly_volume,target=//app nginx
-#docker run -d --rm --name my_cont_readonly -v readonly_volume:/app nginx
+docker run -d --name my_cont_readonly --mount source=readonly_volume,target=/app,readonly nginx
 sleep 5
-# необходим двойной /
