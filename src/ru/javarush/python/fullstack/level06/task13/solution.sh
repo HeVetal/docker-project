296abacb6960fc88979f7e6d0a4185c496ba78a6
Vetal <heavyve@gmail.com>
2025-09-24 14:24:01 +0300

    JavaRush Docker level06 task13

--- src/ru/javarush/python/fullstack/level06/task13/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task13/solution.sh
@@ -4,14 +4,13 @@
 docker volume create app_data
 
 # Восстановите данные из бэкапа в том app_data
-#docker run --rm -v app_data://data -v //path/to/backup:/backup busybox tar xzf //backup/app_data_backup.tar.gz -C /data
-docker run --rm -v app_data:/data -v "E:\MyProjects\PycharmProjects\docker-project\src\ru\javarush\python\fullstack\level06\task13\backup:/backup" busybox sh -c "rm -rf /data/* && tar -xzf /backup/app_data_backup.tar.gz -C /data"
+docker run --rm -v app_data:/data -v "$(pwd)":/backup busybox tar -xzf /backup/app_data_backup.tar.gz -C /data
 
 # Запустите контейнер с Nginx, монтируя восстановленный том app_data для использования данных
-docker run -d --rm --name my_backup -p 8080:80 -v app_data://data nginx
+docker run -d --name my_backup -p 8080:80 -v app_data:/data nginx
 
 # Проверьте, что контейнер запущен, и данные из тома app_data используются
-docker inspect my_backup
-docker exec -it my_backup bash
-docker volume inspect app_data
+docker ps
+
 sleep 10
+
