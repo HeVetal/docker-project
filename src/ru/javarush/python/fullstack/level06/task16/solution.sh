25cf170832967c99027a421e3468fa79fa8303fe
Vetal <heavyve@gmail.com>
2025-09-29 11:49:32 +0300

    JavaRush Docker level06 task16

--- src/ru/javarush/python/fullstack/level06/task16/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task16/solution.sh
@@ -1,12 +1,9 @@
 # Создаем директорию для веб-контента
-#mkdir -p ./web_content
-mkdir -p web_content
+mkdir -p ./web_content
 
 # Создайте простой веб-страничный файл index.html
-#echo "<html><body><h1>Hello from Docker!</h1></body></html>" > ./web_content/index.html
-echo "<html><body><h1>Hello from Docker\!</h1></body></html>" > web_content/index.html
+echo "<html><body><h1>Hello from Docker!</h1></body></html>" > ./web_content/index.html
 
 # Запускаем контейнер Nginx с монтированием директории
-#docker run -d --name my_web_content -p 8080:80 -v /path/on/host:/path/in/web_content nginx
-docker run -d --rm --name my_web_content -p 8080:80 -v /$(pwd)/web_content:/usr/share/nginx/html nginx
+docker run -d --name my_web_content -p 8080:80 -v /$(pwd)/web_content:/usr/share/nginx/html nginx
 sleep 5
