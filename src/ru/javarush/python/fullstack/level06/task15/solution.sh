fb284a40a4394e4ca6145ed9fcac8687b9781212
Vetal <heavyve@gmail.com>
2025-09-29 11:49:26 +0300

    JavaRush Docker level06 task15

--- src/ru/javarush/python/fullstack/level06/task15/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task15/solution.sh
@@ -1,8 +1,5 @@
 # Директория для хранения бэкапов
-#BACKUP_DIR="/path/to/backup_directory"
-#BACKUP_DIR="C:\Users\user\IdeaProjects\docker-project\src\ru\javarush\python\fullstack\level06\task15\docker_backups"
-BACKUP_DIR="/c/Users/user/IdeaProjects/docker-project/src/ru/javarush/python/fullstack/level06/task15/docker_backups"
-#mkdir -p docker_backups
+BACKUP_DIR="/path/to/backup_directory"
 
 # Название тома, который нужно бэкапить
 VOLUME_NAME="app_data"
@@ -14,9 +11,7 @@ mkdir -p $BACKUP_DIR
 BACKUP_FILE="$BACKUP_DIR/${VOLUME_NAME}_backup_$(date +%Y%m%d_%H%M).tar.gz"
 
 # Создайте бэкап тома app_data
-#docker run --rm -v $VOLUME_NAME:/data -v $BACKUP_DIR:/backup busybox tar czf /backup/$BACKUP_FILE -C /data .
-#docker run --rm -v ${VOLUME_NAME}:/data -v ${BACKUP_DIR}:/backup busybox tar czf /backup/${BACKUP_FILE} -C /data .
-docker run --rm -v ${VOLUME_NAME}:/data -v ${BACKUP_DIR}:/backup busybox tar czf "/backup/${BACKUP_FILE}" -C /data .
+docker run --rm -v $VOLUME_NAME:/data -v $BACKUP_DIR:/backup busybox tar -czf "/backup/$(basename "$BACKUP_FILE")" -C /data .
 
 # Удалите бэкапы, которым больше 7 дней
 find $BACKUP_DIR -type f -name "${VOLUME_NAME}_backup_*.tar.gz" -mtime +7 -exec rm -f {} \;
