a95ba8ff1dab1e9e24c9af37e0f2b7fc46f48822
Vetal <heavyve@gmail.com>
2025-09-29 11:49:43 +0300

    JavaRush Docker level06 task18

--- src/ru/javarush/python/fullstack/level06/task18/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task18/solution.sh
@@ -1,4 +1,3 @@
 # Запустите контейнер, смонтировав директорию с хоста в контейнер с правами только для чтения
-#docker run -d --name my_mount_cont --mount type=bind,source=C:\data,target=/app,readonly nginx
-docker run -d --name my_mount_cont --mount type=bind,source=/c/data,target=/app,readonly nginx
+docker run -d --name my_mount_cont --mount type=bind,source=/data,target=/app,readonly nginx
 sleep 5
