62597c56b4518c840249ff99e1dfe5f1c07a8789
Vetal <heavyve@gmail.com>
2025-09-30 16:18:09 +0300

    JavaRush Docker level06 task20

--- src/ru/javarush/python/fullstack/level06/task20/setup.sh
+++ src/ru/javarush/python/fullstack/level06/task20/setup.sh
@@ -1,12 +1,9 @@
 docker swarm init
 docker swarm init --force-new-cluster 2>/dev/null || echo "Swarm уже инициализирован"
 # Создайте конфигурацию в Docker с помощью команды docker config create
-
-
 docker config create nginx_config ./nginx.conf
 docker config ls
 
 # Создайте сервис Nginx, который будет использовать эту конфигурацию
-docker service create --name nginx --config source=nginx_config,target=/etc/nginx/nginx.conf -p 8080:80 nginx
+docker service create --name nginx --config source=nginx_config,target=/etc/nginx/nginx.conf -p 8080:80 nginx:alpine
 sleep 5
-#not print :     return 200 'Hello from Nginx in Docker!';
