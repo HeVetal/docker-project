9ff922221fcfa18d079ab916fb5a6d3307f8ade1
Vetal <heavyve@gmail.com>
2025-09-30 16:18:03 +0300

    JavaRush Docker level06 task19

--- src/ru/javarush/python/fullstack/level06/task19/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task19/solution.sh
@@ -1,20 +1,14 @@
 # Создаем директорию на хосте для монтирования, если она не существует
-mkdir -p my_dir
+mkdir -p /my_dir
 
 # Записываем тестовый файл в директорию на хосте
 echo "test" > my_dir/my_test_file.txt
 
 # Запускаем контейнер с монтированной директорией
-#docker run -d --name cont_dir_test -v my_dir:/usr/share/nginx/html nginx
-#winpty docker run -d --name cont_dir_test -v c\Users\user\IdeaProjects\docker-project\src\ru\javarush\python\fullstack\level06\task19\my_dir\:/usr/share/nginx/html nginx
-docker run -d --name cont_dir_test -v /$(pwd)/my_dir:/usr/share/nginx/html nginx
-sleep 3
+docker run -d --name cont_dir_test -v /my_dir:/data ubuntu tail -f dev/null
 
 # Проверяем содержимое смонтированной директории внутри контейнера
-#winpty docker exec -it cont_dir_test ls /usr/share/nginx/html
-docker exec cont_dir_test ls /usr/share/nginx/html
+winpty docker exec cont_dir_test ls /data
 
 # Выводим содержимое тестового файла внутри контейнера
-#winpty docker exec cont_dir_test cat C:\Users\user\IdeaProjects\docker-project\src\ru\javarush\python\fullstack\level06\task19\my_dir\my_test_file.txt
-docker exec cont_dir_test cat /usr/share/nginx/html/my_test_file.txt
-sleep 5
+winpty docker exec cont_dir_test cat /data/my_test_file.txt
