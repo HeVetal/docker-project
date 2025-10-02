295444d144bd0d4f2eeebf528827862e3bb62fb7
Vetal <heavyve@gmail.com>
2025-10-02 14:01:03 +0300

    JavaRush Docker level06 task24

--- src/ru/javarush/python/fullstack/level06/task24/solution.sh
+++ src/ru/javarush/python/fullstack/level06/task24/solution.sh
@@ -4,4 +4,18 @@ docker-volume-backup backup my_volume my_backup.tar.gz
 # Проверьте, что резервная копия создана
 ls -la my_backup.tar.gz
 
-sleep 5
+# Восстановите том my_volume из резервной копии my_backup.tar.gz
+docker-volume-backup restore my_backup.tar.gz my_volume
+
+# Проверьте, что том восстановлен
+docker volume ls | grep my_volume
+
+#docker run --rm \
+#   -v my_volume:/volume \
+#   -v $(pwd):/backup \
+#   loomchild/volume-backup backup my_backup.tar.gz
+#
+#docker run --rm \
+#   -v my_volume:/volume \
+#   -v $(pwd):/backup \
+#   loomchild/volume-backup restore my_backup.tar.gz
